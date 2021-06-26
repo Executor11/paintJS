158006762a6a2f454712bff3141a0cc602a499cb
Leonid Zakhariants <leonid110400@gmail.com>
2021-06-26 19:46:48 +0400

    fix range, now we can change line width whith range

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 // declarations
 const controlsColors = document.querySelectorAll(".controls__colors");
+const rangeInput = document.querySelector(".range");
 
 // canvas
 const canvas = document.querySelector(".canvas");
@@ -8,15 +9,15 @@ const ctx = canvas.getContext("2d");
 canvas.width = 700;
 canvas.height = 600;
 
-ctx.lineWidth = 5;
-ctx.strokeStyle = "red";
+ctx.lineWidth = 3;
+ctx.strokeStyle = "black";
 
 //painting
 let painting = false;
 
 // apply colors to items
 const colors = ["red", "green", "blue", "yellow", "black"];
-[...controlsColors].map((e, i) => {
+[...controlsColors].forEach((e, i) => {
   e.style.backgroundColor = colors[i];
   e.addEventListener("click", () => (ctx.strokeStyle = colors[i]));
 });
@@ -32,8 +33,12 @@ const onMouseMove = (e) => {
     : (ctx.lineTo(x, y), ctx.stroke());
 };
 
+// fix range for drawing lineWidth
+const drawingLineWidth = (e) => (ctx.lineWidth = e.target.value);
+
 //canvas events
 if (canvas) {
+  rangeInput.addEventListener("change", drawingLineWidth);
   canvas.addEventListener("mousemove", onMouseMove);
   canvas.addEventListener("mousedown", () => (painting = true));
   canvas.addEventListener("mouseup", () => (painting = false));
